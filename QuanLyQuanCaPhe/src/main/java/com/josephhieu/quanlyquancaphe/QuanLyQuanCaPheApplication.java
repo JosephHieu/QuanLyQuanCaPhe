6a4142976bf3e6ff8f0b3bec4c1e9732414cda38
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-10-26 09:06:56 +0700

    setup project

---
+++ QuanLyQuanCaPhe/src/main/java/com/josephhieu/quanlyquancaphe/QuanLyQuanCaPheApplication.java
@@ -0,0 +1,13 @@
+package com.josephhieu.quanlyquancaphe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyQuanCaPheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyQuanCaPheApplication.class, args);
+    }
+
+}
---
+++ QuanLyQuanCaPhe/src/test/java/com/josephhieu/quanlyquancaphe/QuanLyQuanCaPheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephhieu.quanlyquancaphe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyQuanCaPheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
